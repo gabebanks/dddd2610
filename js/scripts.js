2faa6c7a096b20fd3901f56797ba3c21d4c1d89f
Gabe Bankhead <122393077+gabebanks@users.noreply.github.com>
2024-10-14 09:39:51 -0600

    Assignment Upload
    07 University Admissions

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-console.log('I am connected')
-
-const btnElement = document.querySelector('#primaryNav > button');
-const navElement = document.querySelector('#primaryNav > ul');
-
-btnElement.addEventListener('click', () => {
-    btnElement.classList.toggle('open');
-    navElement.classList.toggle('open');
-});
