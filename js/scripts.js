ce97187acc1cf7379b124450f751b870f222f912
Gabe Bankhead <122393077+gabebanks@users.noreply.github.com>
2024-11-23 23:55:44 -0700

    Uploading Assignment
    Module 9 Assignment / Publish 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-const btnElement = document.querySelector('#hamburgerBtn');
-const navElement = document.querySelector('#primaryNav');
+const navElement = document.querySelector('#theNav');
+const btnElement = document.querySelector('#theButton');
 
 btnElement.addEventListener('click', () => {
     btnElement.classList.toggle('open');
     navElement.classList.toggle('open');
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // start of autoplay
+    autoplay: {
+        delay: 5000,
+    },
+  });
