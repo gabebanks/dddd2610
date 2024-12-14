87bc440f9193fc2ad6504c950a21cc5d0a10d43c
Gabe Bankhead <122393077+gabebanks@users.noreply.github.com>
2024-12-13 22:00:36 -0700

    Capstone Project
    Assignment Upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,9 @@
-// get me a list of all the items to watch
-const myListOfItems = document.querySelectorAll('section')
+console.log('I am connected');
 
-// a comma deliniated list of name/value pairs controlling how the observer works
-let observerOptions = {
-    //null is the default and references the viewport
-    root: null,
-    //alters the viewport. negative values decrease the size.
-    rootMargin: '0px 0px -30px 0px',
-    //0 is barely showing, 1 is fully showing
-    threshold: .25
-  }
+const btnElement = document.querySelector('#hamburgerBtn button'); // Ensure this targets the button inside #hamburgerBtn
+const navElement = document.querySelector('#primaryNav > ul');
 
-// AllItems is a list of all elements being watched
-const myObserver = new IntersectionObserver(allItems => {
-    allItems.forEach(singleItem => {
-        if (singleItem.isIntersecting){
-            hiliteNav(singleItem.target)
-        }
-    })
-}, observerOptions)
-
-// function to hilight the current navigation items
-function hiliteNav(x) {
-	document.querySelector('.active').classList.remove('active');
-	let theid = x.getAttribute('id');
-	let newActiveLink = document.querySelector(`[href="#${theid}"]`)
-	newActiveLink.parentElement.classList.add('active');
-}
-
-//call the function for each element in the list
-myListOfItems.forEach(item => {
-    myObserver.observe(item)
+btnElement.addEventListener('click', () => {
+    btnElement.classList.toggle('open');  // Toggles 'open' class on button
+    navElement.classList.toggle('open');  // Toggles 'open' class on menu
 });
